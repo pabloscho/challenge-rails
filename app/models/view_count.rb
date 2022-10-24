3fcb9e133e42b82fed4f26477ae714afe6342db4
Pablo <pabloscho@gmail.com>
2022-10-24 06:35:52 -0300

    Display view_count in home

--- app/models/view_count.rb
+++ app/models/view_count.rb
@@ -2,5 +2,11 @@ class ViewCount < ApplicationRecord
   def self.increment(path)
     view_count = ViewCount.find_or_create_by(path: path)
     view_count.update_attribute(:views, view_count.views + 1)
+
+    @total = view_count.views
+  end
+
+  def self.total
+    @total
   end
 end
